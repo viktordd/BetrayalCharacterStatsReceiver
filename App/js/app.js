7aeb0d1fe882f587e0d65fbf131b23b85ea0fb12
viktordd <Viktordd@gmail.com>
2015-03-15 13:05:35 -0400

    Added assets

---
+++ App/js/app.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+
+angular.module('betrayalCharacterStatsReceiver', ['ngRoute', 'bcsrControllers'])
+    .config([
+        '$routeProvider', function($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'views/main.html',
+                    controller: 'mainCtrl'
+                })
+                .otherwise({
+                    redirectTo: '/'
+                });
+        }
+    ]);
---
+++ App/js/directives/char.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+
+angular.module('betrayalCharacterStatsReceiver').directive('bcsrChar', function() {
+    return {
+        restrict: 'E',
+        scope: {
+            char: '='
+        },
+        templateUrl: 'templates/char.html',
+        link: function(scope, element) {
+            scope.getClass = function(pos) {
+                return 'p-' + pos;
+            }
+            scope.min = function() {
+                scope.char.might = 0;
+                scope.char.speed = 0;
+                scope.char.sanity = 0;
+                scope.char.knowledge = 0;
+            }
+            scope.max = function() {
+                scope.char.might = 8;
+                scope.char.speed = 8;
+                scope.char.sanity = 8;
+                scope.char.knowledge = 8;
+            }
+        }
+    }
+});
