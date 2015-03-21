493ba83b24f42273b1f1125d19d8c75f51866416
viktordd <Viktordd@gmail.com>
2015-03-21 16:07:33 -0400

    added test user

--- App/js/controllers/main.js
+++ App/js/controllers/main.js
@@ -5,38 +5,38 @@ angular.module('betrayalCharacterStatsReceiver').controller('mainCtrl', ['$scope
 
         $scope.chars = {
             data: [
-                //{
-                //    id: '1',
-                //    char: 'blue_madame_zostra',
-                //    playerName: 'Test Player',
-                //    speed: 3,
-                //    might: 4,
-                //    sanity: 3,
-                //    knowledge: 4
-                //},
+                {
+                    id: '1',
+                    char: 'blue_madame_zostra',
+                    playerName: 'Test Player',
+                    speed: 3,
+                    might: 4,
+                    sanity: 3,
+                    knowledge: 4
+                },
             ]
         };
 
         cast.receiver.logger.setLevelValue(0);
-        window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
+        var castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
         console.log('Starting Receiver Manager');
 
         // handler for the 'ready' event
         castReceiverManager.onReady = function(event) {
             console.log('Received Ready event: ' + JSON.stringify(event.data));
-            window.castReceiverManager.setApplicationState('Application status is ready...');
+            castReceiverManager.setApplicationState('Application status is ready...');
         };
 
         // handler for 'senderconnected' event
         castReceiverManager.onSenderConnected = function(event) {
             console.log('Received Sender Connected event: ' + event.data);
-            console.log(window.castReceiverManager.getSender(event.data).userAgent);
+            console.log(castReceiverManager.getSender(event.data).userAgent);
         };
 
         // handler for 'senderdisconnected' event
         castReceiverManager.onSenderDisconnected = function(event) {
             console.log('Received Sender Disconnected event: ' + event.data);
-            if (window.castReceiverManager.getSenders().length == 0) {
+            if (castReceiverManager.getSenders().length == 0) {
                 window.close();
             }
         };
@@ -48,7 +48,7 @@ angular.module('betrayalCharacterStatsReceiver').controller('mainCtrl', ['$scope
         //};
 
         // create a CastMessageBus to handle messages for a custom namespace
-        window.messageBus = window.castReceiverManager.getCastMessageBus('urn:x-cast:com.google.cast.betrayalCharacterStats');
+        window.messageBus = castReceiverManager.getCastMessageBus('urn:x-cast:com.google.cast.betrayalCharacterStats');
 
         // handler for the CastMessageBus message event
         window.messageBus.onMessage = function(event) {
@@ -59,7 +59,7 @@ angular.module('betrayalCharacterStatsReceiver').controller('mainCtrl', ['$scope
             window.messageBus.send(event.senderId, event.data);
         }
         // initialize the CastReceiverManager with an application status message
-        window.castReceiverManager.start({ statusText: 'Application is starting' });
+        castReceiverManager.start({ statusText: 'Application is starting' });
         console.log('Receiver Manager started');
     }
 ]);
