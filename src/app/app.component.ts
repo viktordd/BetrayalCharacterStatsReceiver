7aa69f200baa8f17fc1db6edb5e40d3d35da254b
Viktor Dikov <viktordd@gmail.com>
2017-11-09 21:37:03 -0500

    Created game manager.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { CastReceiverManagerService } from './services/cast-receiver-manager.service';
-import { MessageBusService } from './services/message-bus.service';
+import { GameManagerService } from './services/game-manager.service';
 import { Player } from './players/player.model';
 
 @Component({
@@ -16,62 +15,10 @@ export class AppComponent implements OnInit {
   private players: Player[] = [];
 
   constructor(
-    private messageBusService: MessageBusService,
-    private castReceiverManagerService: CastReceiverManagerService) { }
+    private gameManagerService: GameManagerService) { }
 
   ngOnInit() {
-    this.castReceiverManagerService.onSenderConnected.subscribe(id => this.onSenderConnected(id));
-    this.castReceiverManagerService.onSenderDisconnected.subscribe(id => this.onSenderDisconnected(id));
-    this.messageBusService.onMessage.subscribe(event => this.onMessage(event));
-
-    this.messageBusService.init();
-  }
-
-  private onSenderConnected(id: string) {
-    const player = this.findPlayerById(id);
-    if (!player) {
-      this.players.push(new Player(id));
-    }
-  }
-
-  private onSenderDisconnected(id: string) {
-    const pos = this.findPlayerPosById(id);
-    if (pos >= 0) {
-      this.players.splice(pos, 1);
-    }
-  }
-
-  private onMessage(player: Player) {
-    const pos = this.findPlayerPosById(player.id);
-
-    if (pos >= 0) {
-      Object.assign(this.players[pos], player);
-    } else {
-      this.players.push(player);
-    }
-  }
-
-  private findPlayerById(id: string) {
-    for (let i = 0; i < this.players.length; i++) {
-      const p = this.players[i];
-      if (p.id === id) {
-        return p;
-      }
-    }
-    return null;
-  }
-
-  private findPlayerPosById(id: string) {
-    for (let i = 0; i < this.players.length; i++) {
-      const p = this.players[i];
-      if (p.id === id) {
-        return i;
-      }
-    }
-    return -1;
-  }
-
-  private trackByPlayers(index: number, player: Player) {
-    return player.id;
+    this.gameManagerService.init();
+    this.players = this.gameManagerService.Players;
   }
 }
---
+++ src/app/services/game-manager.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+
+import { CastReceiverManagerService } from './cast-receiver-manager.service';
+import { MessageBusService } from './message-bus.service';
+import { Player } from '../players/player.model';
+
+
+@Injectable()
+export class GameManagerService {
+    private players: Player[] = [];
+
+    public get Players(): Player[] {
+        return this.players;
+    }
+
+    constructor(
+        private messageBusService: MessageBusService,
+        private castReceiverManagerService: CastReceiverManagerService) { }
+
+    public init() {
+        this.castReceiverManagerService.onSenderConnected.subscribe(id => this.onSenderConnected(id));
+        this.castReceiverManagerService.onSenderDisconnected.subscribe(id => this.onSenderDisconnected(id));
+        this.messageBusService.onMessage.subscribe(event => this.onMessage(event));
+
+        this.messageBusService.init();
+    }
+
+    private onSenderConnected(id: string) {
+        const player = this.findPlayerById(id);
+        if (!player) {
+            this.players.push(new Player(id));
+        }
+    }
+
+    private onSenderDisconnected(id: string) {
+        const pos = this.findPlayerPosById(id);
+        if (pos >= 0) {
+            this.players.splice(pos, 1);
+        }
+    }
+
+    private onMessage(player: Player) {
+        const pos = this.findPlayerPosById(player.id);
+
+        if (pos >= 0) {
+            Object.assign(this.players[pos], player);
+        } else {
+            this.players.push(player);
+        }
+    }
+
+    private findPlayerById(id: string) {
+        for (let i = 0; i < this.players.length; i++) {
+            const p = this.players[i];
+            if (p.id === id) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    private findPlayerPosById(id: string) {
+        for (let i = 0; i < this.players.length; i++) {
+            const p = this.players[i];
+            if (p.id === id) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private trackByPlayers(index: number, player: Player) {
+        return player.id;
+    }
+}
